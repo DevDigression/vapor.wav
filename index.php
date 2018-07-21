b9640fbe62800fce2b3c1f35b6d7b73331e5b235
Chris Rodgers <devdigression@gmail.com>
2018-07-21 16:03:54 -0400

    dashboard setup

--- index.php
+++ index.php
@@ -18,6 +18,24 @@ if (isset($_SESSION['userLoggedIn'])) {
 	<title>V A P O R . w a v</title>
 </head>
 <body>
+	<div id="dashboard-container">
+		<div id="top-section-container">
+			<div id="left-navbar-container">
+				<nav class="navbar">
+					<a href="index.php" class="logo"><img src="./assets/images/icons/vapordotwav-logo.png" /></a>
+					<div class="group">
+						<div class="nav-item"><a href="search.php" class="nav-item-link">Search
+							<img src="./assets/images/icons/search.png" class="icon" alt="Search" /></a>
+						</div>
+					</div>
+					<div class="group">
+						<div class="nav-item"><a href="browse.php" class="nav-item-link">Browse</a></div>
+						<div class="nav-item"><a href="your-music.php" class="nav-item-link">Your Music</a></div>
+						<div class="nav-item"><a href="user.php" class="nav-item-link">User Name</a></div>
+					</div>
+				</nav>
+			</div>
+		</div>
 	<div id="now-playing-bar-container">
 		<div id="now-playing-bar">
 			<div id="now-playing-left">
@@ -78,6 +96,7 @@ if (isset($_SESSION['userLoggedIn'])) {
 				</div>
 			</div>
 		</div>
+	</div>
 	</div>
 </body>
 </html>
