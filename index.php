0f52a9c92af906ed99a12a6cdac48ba6adf877dd
Chris Rodgers <devdigression@gmail.com>
2018-07-22 21:14:06 -0400

    added song data to database

--- index.php
+++ index.php
@@ -20,21 +20,7 @@ if (isset($_SESSION['userLoggedIn'])) {
 <body>
 	<div id="dashboard-container">
 		<div id="top-section-container">
-			<div id="left-navbar-container">
-				<nav class="navbar">
-					<a href="index.php" class="logo"><img src="./assets/images/icons/vapordotwav-logo.png" /></a>
-					<div class="group">
-						<div class="nav-item"><a href="search.php" class="nav-item-link">Search
-							<img src="./assets/images/icons/search.png" class="icon" alt="Search" /></a>
-						</div>
-					</div>
-					<div class="group">
-						<div class="nav-item"><a href="browse.php" class="nav-item-link">Browse</a></div>
-						<div class="nav-item"><a href="your-music.php" class="nav-item-link">Your Music</a></div>
-						<div class="nav-item"><a href="user.php" class="nav-item-link">User Name</a></div>
-					</div>
-				</nav>
-			</div>
+			<?php include("./includes/navbar-container.php"); ?>
 		</div>
 	<div id="now-playing-bar-container">
 		<div id="now-playing-bar">
