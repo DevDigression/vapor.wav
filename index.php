43bd556c8d8465be7cfbad8e5e15a6d5120f2b91
Chris Rodgers <devdigression@gmail.com>
2018-07-23 06:37:52 -0400

    styled suggested music

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php include("./includes/header.php"); ?>	
 
+<h1 class="page-heading-big">You Might Also Like</h1>
+
+<div class="grid-view-container">
+	<?php $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+		while ($row = mysqli_fetch_array($albumQuery)) {
+			echo "<div class='grid-view-item'>
+			<a href='album.php?id=" . $row['id'] . "'>
+				<img src='" . $row['artworkPath'] . "' />
+		
+				<div class='grid-view-info'>"
+					. $row['title'] .	
+				"</div>
+			</a>
+				</div>";
+		}
+	?>
+</div>
+
 <?php include("./includes/footer.php"); ?>
