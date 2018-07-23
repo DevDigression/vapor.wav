700079aff2b704217ddb49bcb1d599b7814b2f85
Chris Rodgers <devdigression@gmail.com>
2018-07-22 21:32:36 -0400

    created header and footer

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<?php 
+include("includes/config.php");
+
+// code to manually log out - uncomment below to log out
+// session_destroy();
+
+if (isset($_SESSION['userLoggedIn'])) {
+	$userLoggedIn = $_SESSION['userLoggedIn'];
+} else {
+	header("Location: register.php");
+}
+
+?>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="./assets/css/style.css">
+	<title>V A P O R . w a v</title>
+</head>
+<body>
+	<div id="dashboard-container">
+		<div id="top-section-container">
+			<?php include("./includes/navbar-container.php"); ?>
+
+			<div id="main-view-container">
+				<div id="main-content">
