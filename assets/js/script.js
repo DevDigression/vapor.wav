e70f8e82046846626aab802ac20c4dfd50279152
Chris Rodgers <devdigression@gmail.com>
2018-09-01 22:16:06 -0400

    update progress bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,53 @@
 var currentPlaylist = [];
 var audioElement;
 
-function Audio () {
+function formatTime(seconds) {
+	var time = Math.round(seconds);
+	var minutes = Math.floor(time / 60);
+	var seconds = time - minutes * 60;
+
+	var extraZero = seconds < 10 ? "0" : "";
+
+	return minutes + ":" + extraZero + seconds;
+}
+
+function updateTimeProgressBar(audio) {
+	var current = document.querySelector(".progress-time.current");
+	current.textContent = formatTime(audio.currentTime);
+	var remaining = document.querySelector(".progress-time.remaining");
+	remaining.textContent = formatTime(audio.duration - audio.currentTime);
+
+	var percentComplete = audio.currentTime / audio.duration * 100;
+	var progress = document.querySelector(".playback-bar .progress");
+	progress.style.width = percentComplete + "%";
+}
+
+function Audio() {
 	this.currentlyPlaying;
-	this.audio = document.createElement('audio');
+	this.audio = document.createElement("audio");
+
+	this.audio.addEventListener("canplay", function() {
+		var duration = formatTime(this.duration);
+		var remainingTime = document.querySelector(".progress-time.remaining");
+		remainingTime.textContent = duration;
+	});
+
+	this.audio.addEventListener("timeupdate", function() {
+		if (this.duration) {
+			updateTimeProgressBar(this);
+		}
+	});
 
 	this.setTrack = function(track) {
 		this.currentlyPlaying = track;
 		this.audio.src = track.path;
-	}
+	};
 
 	this.play = function() {
 		this.audio.play();
-	}
+	};
 
 	this.pause = function() {
 		this.audio.pause();
-	}
+	};
 }
